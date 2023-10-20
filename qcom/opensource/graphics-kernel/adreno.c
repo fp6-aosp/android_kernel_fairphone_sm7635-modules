ea911f1eb7da0d1e048b5e8112a95f8bc89a8a8f
Lynus Vaz <quic_lvaz@quicinc.com>
2023-10-20 10:26:28 -0700

    msm: kgsl: Use the devfreq governors inside the kernel tree
    If the kernel enables in-tree versions of the Adreno GPU and bandwidth
    governors, use these instead of the kgsl code.
    
    Change-Id: I996f0d50acf88dbbd95a9b7f33221c6748052310
    Signed-off-by: Lynus Vaz <quic_lvaz@quicinc.com>

--- qcom/opensource/graphics-kernel/adreno.c
+++ qcom/opensource/graphics-kernel/adreno.c
@@ -3871,7 +3871,7 @@ module_exit(kgsl_3d_exit);
 
 MODULE_DESCRIPTION("3D Graphics driver");
 MODULE_LICENSE("GPL v2");
-MODULE_SOFTDEP("pre: arm_smmu nvmem_qfprom socinfo");
+MODULE_SOFTDEP("pre: arm_smmu nvmem_qfprom socinfo governor_msm_adreno_tz governor_gpubw_mon");
 #if (KERNEL_VERSION(5, 18, 0) <= LINUX_VERSION_CODE)
 MODULE_IMPORT_NS(DMA_BUF);
 #endif
