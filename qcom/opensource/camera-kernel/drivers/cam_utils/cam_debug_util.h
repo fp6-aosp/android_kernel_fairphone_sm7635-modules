234e3c43956516d19d98ed6fcb25dcbaebd2da18
Vijay Kumar Tumati <quic_vtumati@quicinc.com>
2024-03-25 11:11:56 -0700

    msm: camera: common: Fix compilation issue with LTS 6.6.17
    Fix the issues by using right size in snprintf that
    includes the null terminator. Additionally, get rid of
    the redundant dump utility in the TPG driver and add a
    new log type for the dump in the IO util driver.
    
    CRs-Fixed: 3768939
    
    Signed-off-by: Satya Durga Srinivasu Prabhala <quic_satyap@quicinc.com>
    Signed-off-by: Vijay Kumar Tumati <quic_vtumati@quicinc.com>
    Change-Id: Ia79b097c7525649990be184445a5973d7c1870b3

--- qcom/opensource/camera-kernel/drivers/cam_utils/cam_debug_util.h
+++ qcom/opensource/camera-kernel/drivers/cam_utils/cam_debug_util.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _CAM_DEBUG_UTIL_H_
@@ -63,6 +63,7 @@ enum cam_debug_module_id {
 	CAM_DMA_FENCE,           /* bit 34 */
 	CAM_SENSOR_UTIL,         /* bit 35 */
 	CAM_SYNX,                /* bit 36 */
+	CAM_IO_DUMP,             /* bit 37 */
 	CAM_DBG_MOD_MAX
 };
 
@@ -123,6 +124,7 @@ static const char *cam_debug_mod_name[CAM_DBG_MOD_MAX] = {
 	[CAM_DMA_FENCE]   = "CAM-DMA-FENCE",
 	[CAM_SENSOR_UTIL] = "CAM-SENSOR-UTIL",
 	[CAM_SYNX]        = "CAM_SYNX",
+	[CAM_IO_DUMP]     = "CAM-IO-DUMP",
 };
 
 #define ___CAM_DBG_MOD_NAME(module_id)                                      \
@@ -163,6 +165,7 @@ __builtin_choose_expr(((module_id) == CAM_TPG), "CAM-TPG",                  \
 __builtin_choose_expr(((module_id) == CAM_DMA_FENCE), "CAM-DMA-FENCE",      \
 __builtin_choose_expr(((module_id) == CAM_SENSOR_UTIL), "CAM-SENSOR-UTIL",      \
 __builtin_choose_expr(((module_id) == CAM_SYNX), "CAM-SYNX",                \
+__builtin_choose_expr(((module_id) == CAM_IO_DUMP), "CAM-IO-DUMP",          \
 "CAMERA")))))))))))))))))))))))))))))))))))))
 
 #define CAM_DBG_MOD_NAME(module_id) \
