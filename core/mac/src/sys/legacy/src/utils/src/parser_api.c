0a7f19f811cc89d3973ab73a6d50537665fb67c2
Surya Prakash Sivaraj <suryapra@qti.qualcomm.com>
2024-05-28 12:02:33 +0530

    qcacld-3.0: Increase the akm suites count to 8 in RSN IE
    The 802.11 standard doesn't limit the number of AKM suites that
    can be advertised in RSN IE. The restriction is there only
    on the total size of the RSN IE. In some cases like FILS,
    the APs could advertise more than 6 AKMs in the RSN IE.
    While parsing beacons of those AP, the frame parser checks
    for maximum of 6 AKMs and ignores the RSN IE if the AKM
    count is greater than 6. This causes install key flag not
    sent to firmware and the data packets are going out unencrypted.
    
    Increase the number of akm suites allowed to 8 in RSN IE
    Enhance few debug prints for FILS.
    
    Change-Id: I91587f2af80a232ebff3053fc4b912283191769d
    CRs-Fixed: 3763552

--- core/mac/src/sys/legacy/src/utils/src/parser_api.c
+++ core/mac/src/sys/legacy/src/utils/src/parser_api.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -5078,6 +5078,8 @@ QDF_STATUS sir_parse_beacon_ie(struct mac_context *mac,
 	if (pBies->RSN.present) {
 		pBeaconStruct->rsnPresent = 1;
 		convert_rsn(mac, &pBeaconStruct->rsn, &pBies->RSN);
+	} else {
+		pe_debug("RSN IE is not present");
 	}
 
 	if (pBies->WPA.present) {
