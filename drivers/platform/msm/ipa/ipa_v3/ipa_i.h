32e90f12f82beb85b733f4fcfc895c0b871810bd
Pavan Kumar M <quic_rpavan@quicinc.com>
2024-06-19 17:37:11 +0530

    msm: ipa3: Log AP wake up source pipe
    Log the wake up source pipe in scenarios of device exiting
    suspend mode.
    
     •  Wake up triggered by ipa voting
     •  First instance of ipa voting after device is out of suspend mode.
    
    Change-Id: I7611461d924a30f664dd7d89759a5b33ebd20baa
    Signed-off-by: Pavan Kumar M <quic_rpavan@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipa_i.h
@@ -2644,6 +2644,7 @@ struct ipa3_context {
 	struct ipa3_page_recycle_stats prev_low_lat_data_recycle_stats;
 	struct mutex recycle_stats_collection_lock;
 	struct mutex ssr_lock;
+	atomic_t is_suspend_mode_enabled;
 };
 
 struct ipa3_plat_drv_res {
