ebd1a7579af66090370594c29f6c974ff3f7bb40
Pragaspathi Thilagaraj <quic_tpragasp@quicinc.com>
2024-08-04 23:22:27 +0530

    qcacld-3.0: Disallow TDLS with few unsupported peers
    Some IOT TDLS capable devices try cause frequent TDLS teardown
    immediately after successful TDLS setup.
    
    So to avoid such TDLS setup, when repeated STA kickout is
    received from an IOT peer, then set the TDLS capability of
    the PSOC TDLS peer as not capable to avoid it from
    establishing further TDLS connection.
    
    Change-Id: I1bf7bfc95ff607d349c78c8778748a9a7529c765
    CRs-Fixed: 3885984

--- core/mac/src/pe/lim/lim_link_monitoring_algo.c
+++ core/mac/src/pe/lim/lim_link_monitoring_algo.c
@@ -46,6 +46,7 @@
 #include "lim_session.h"
 #include "lim_ser_des_utils.h"
 #include "wlan_dlm_api.h"
+#include "wlan_tdls_api.h"
 
 /**
  * lim_delete_sta_util - utility function for deleting station context
@@ -114,6 +115,8 @@ static void lim_delete_sta_util(struct mac_context *mac_ctx, tpDeleteStaContext 
 #ifdef FEATURE_WLAN_TDLS
 		if (LIM_IS_STA_ROLE(session_entry) &&
 		    STA_ENTRY_TDLS_PEER == stads->staType) {
+			wlan_tdls_update_peer_kickout_count(session_entry->vdev,
+							    stads->staAddr);
 			/*
 			 * TeardownLink with PEER reason code
 			 * HAL_DEL_STA_REASON_CODE_KEEP_ALIVE means
