arter97-mirror/caf_qca-wifi-host-cmn

qcacld-3.0: Don't allow CSA when STA/P2P-CLI eapol is in progress
Currently, driver does not reject CSA on P2P GO iface when eapol
is in progress for STA/P2P_CLI or NOA is in progress in P2P GO
vdev when CSA receives from northbound. This could lead to delay in
CSA because during STA/p2p-cli connection+eapol, firmware does
NOA of max 2.5 secs on GO iface to ensure the STA connection is
successful. So, GO will not be able to send the CSA frames during
one shot of NOA absent period.

As part of fix, reject CSA if
1. Eapol is in progress on STA/P2P_CLI vdev or
2. NOA is in progress in P2P GO vdev and still CSA request comes
from northbound

CRs-Fixed: 3890142
Change-Id: I6b2bd429ab60cfb596863e402cf32b9aab25e7c6

core/hdd/src/wlan_hdd_hostapd.c
@@ -3757,6 +3757,7 @@ bool hdd_is_sta_connect_or_link_switch_in_prog(struct hdd_context *hdd_ctx)
 	wlan_net_dev_ref_dbgid dbgid = NET_DEV_HOLD_IS_ANY_STA_CONNECTING;
 	struct wlan_hdd_link_info *link_info;
 	bool is_connecting = false, is_switching_link = false;
+	bool key_exchng_in_prog = false;
 
 	if (!hdd_ctx) {
 		hdd_err("HDD context is NULL");
@@ -3777,12 +3778,17 @@ bool hdd_is_sta_connect_or_link_switch_in_prog(struct hdd_context *hdd_ctx)
 			    wlan_hdd_is_link_switch_in_progress(link_info))
 				is_switching_link = true;
 
-			if (!is_connecting && !is_switching_link)
+			key_exchng_in_prog =
+					sme_is_sta_key_exchange_in_progress(
+							hdd_ctx->mac_handle,
+							link_info->vdev_id);
+			if (!is_connecting && !is_switching_link &&
+			    !key_exchng_in_prog)
 				continue;
 
-			hdd_debug("vdev_id %d: connecting %d switching link %d",
+			hdd_debug("vdev_id %d: connecting %d switching link %d key_exchng_in_prog %d",
 				  link_info->vdev_id, is_connecting,
-				  is_switching_link);
+				  is_switching_link, key_exchng_in_prog);
 
 			hdd_adapter_dev_put_debug(adapter, dbgid);
 			if (next_adapter)
@@ -3853,9 +3859,20 @@ int hdd_softap_set_channel_change(struct wlan_hdd_link_info *link_info,
 	 * Eg: The concurrency could be safe when link switch vdev is torn down
 	 * during link switch. However, when the link comes up, the combination
 	 * could be undesired.
+	 *
+	 * When P2P GO + STA/P2P-CLI concurrency arises. Dont allow CSA on GO
+	 * iface while eapol is in progress for STA/P2P-CLI vdev's or noa is in
+	 * progress in p2p go iface. Because for STA/P2P-CLI connection+eapol,
+	 * firmware does NOA(max 2.5 secs) on GO iface and during this time it
+	 * cannot do CSA as it won't be able to send CSA frames during NOA
+	 * period
 	 */
-	if (hdd_is_sta_connect_or_link_switch_in_prog(hdd_ctx)) {
-		hdd_err("Do not allow CSA, STA connect/link switch is in progress");
+	if (hdd_is_sta_connect_or_link_switch_in_prog(hdd_ctx) ||
+	    (adapter->device_mode == QDF_P2P_GO_MODE &&
+	     ucfg_p2p_is_p2p_go_noa_in_progress(hdd_ctx->pdev,
+						link_info->vdev_id))) {
+		hdd_err("vdev %d Do not allow CSA, STA connect/link switch/eapol/noa is in progress",
+			link_info->vdev_id);
 		return -EBUSY;
 	}
 

fp6-aosp/android_kernel_fairphone_sm7635-modules

core/hdd/src/wlan_hdd_hostapd.c
@@ -3459,6 +3459,8 @@ bool hdd_is_any_sta_connecting(struct hdd_context *hdd_ctx)
 	struct hdd_station_ctx *sta_ctx;
 	wlan_net_dev_ref_dbgid dbgid = NET_DEV_HOLD_IS_ANY_STA_CONNECTING;
 	struct wlan_hdd_link_info *link_info;
+	bool is_connecting = false;
+	bool key_exchng_in_prog = false;
 
 	if (!hdd_ctx) {
 		hdd_err("HDD context is NULL");
@@ -3473,10 +3475,20 @@ bool hdd_is_any_sta_connecting(struct hdd_context *hdd_ctx)
 
 		hdd_adapter_for_each_active_link_info(adapter, link_info) {
 			sta_ctx = WLAN_HDD_GET_STATION_CTX_PTR(link_info);
-			if (!hdd_cm_is_connecting(link_info))
+
+			is_connecting = hdd_cm_is_connecting(link_info);
+
+			key_exchng_in_prog =
+					sme_is_sta_key_exchange_in_progress(
+							hdd_ctx->mac_handle,
+							link_info->vdev_id);
+			if (!is_connecting && !key_exchng_in_prog)
 				continue;
 
-			hdd_debug("vdev_id %d: connecting", link_info->vdev_id);
+			hdd_debug("vdev_id %d: connecting %d key_exchng_in_prog %d",
+				  link_info->vdev_id, is_connecting,
+				  key_exchng_in_prog);
+
 			hdd_adapter_dev_put_debug(adapter, dbgid);
 			if (next_adapter)
 				hdd_adapter_dev_put_debug(next_adapter, dbgid);
@@ -3534,9 +3546,20 @@ int hdd_softap_set_channel_change(struct net_device *dev, int target_chan_freq,
 	 * If sta connection is in progress do not allow SAP channel change from
 	 * user space as it may change the HW mode requirement, for which sta is
 	 * trying to connect.
+	 *
+	 * When P2P GO + STA/P2P-CLI concurrency arises. Dont allow CSA on GO
+	 * iface while eapol is in progress for STA/P2P-CLI vdev's or noa is in
+	 * progress in p2p go iface. Because for STA/P2P-CLI connection+eapol,
+	 * firmware does NOA(max 2.5 secs) on GO iface and during this time it
+	 * cannot do CSA as it won't be able to send CSA frames during NOA
+	 * period
 	 */
-	if (hdd_is_any_sta_connecting(hdd_ctx)) {
-		hdd_err("STA connection is in progress");
+	if (hdd_is_any_sta_connecting(hdd_ctx) ||
+	    (adapter->device_mode == QDF_P2P_GO_MODE &&
+	     ucfg_p2p_is_p2p_go_noa_in_progress(hdd_ctx->pdev,
+						link_info->vdev_id))) {
+		hdd_err("vdev %d Do not allow CSA, STA connect/eapol/noa is in progress",
+			link_info->vdev_id);
 		return -EBUSY;
 	}
 