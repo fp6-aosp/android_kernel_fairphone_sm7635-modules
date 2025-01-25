2281ac812ff5628e25a07537d6ebb00056748595
dianlujitao <dianlujitao@lineageos.org>
2025-01-25 23:18:47 +0800

    qcom: audio-kernel: hdmi_dlkm: Include headers from msm_ext_display
    Change-Id: Ic9523330ac62af3e133dc632a7bc0a88b2896caf

--- qcom/opensource/audio-kernel/asoc/codecs/msm_hdmi_codec_rx.c
+++ qcom/opensource/audio-kernel/asoc/codecs/msm_hdmi_codec_rx.c
@@ -11,7 +11,7 @@
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/soc.h>
-#include <linux/soc/qcom/msm_ext_display.h>
+#include <msm_ext_display.h>
 
 #define DRV_NAME "HDMI_codec"
 
